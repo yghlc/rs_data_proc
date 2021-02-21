6a1401e84c249d3573a62662132024910c5fc53e
Lingcao Huang <huanglingcao@gmail.com>
2021-02-21 09:22:33 -0700

    minor fix

--- greenland_permafrost/ortho_to_8bit.sh
+++ greenland_permafrost/ortho_to_8bit.sh
@@ -14,12 +14,12 @@
 out_dir=sub_region_8bit
 rm -r ${out_dir}
 mkdir ${out_dir}
-for tif in $(ls sub_region/*_ortho.tif); do
+for tif in sub_region/*_ortho.tif; do
 
     echo $tif
     filename=$(basename "$tif")
-	extension="${filename##*.}"
-	filename_noext="${filename%.*}"
+#	  extension="${filename##*.}"
+	  filename_noext="${filename%.*}"
 
     out8bit=${out_dir}/${filename_noext}_8bit.tif
     gdal_contrast_stretch -percentile-range 0.02 0.98 ${tif} ${out8bit}
