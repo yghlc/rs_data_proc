6427b4068d2581ac9f84a9af616a72342ea65b3e
Lingcao Huang <huanglingcao@gmail.com>
2021-02-25 09:57:08 -0700

    bug fix

--- tools/backup_curc_scripts.py
+++ tools/backup_curc_scripts.py
@@ -23,19 +23,19 @@ def main():
     sh_list = io_function.get_file_list_by_ext('.sh',org_dir,bsub_folder=True)
     changed_list = []
     for sh in sh_list:
-        bak_path = bak_dir + sh.replace(org_dir,'')
+        new_path = bak_dir + sh.replace(org_dir,'')
         # print(bak_path)
         mo_time = os.path.getmtime(sh)
-        if os.path.isfile(bak_path):
-            bak_mo_time = os.path.getmtime(bak_path)
+        if os.path.isfile(new_path):
+            bak_mo_time = os.path.getmtime(new_path)
             if mo_time == bak_mo_time:
                 continue
 
-        bak_dir = os.path.dirname(bak_path)
-        if os.path.isdir(bak_dir) is False:
-            io_function.mkdir(bak_dir)
+        new_dir = os.path.dirname(new_path)
+        if os.path.isdir(new_dir) is False:
+            io_function.mkdir(new_dir)
 
-        res = os.system('cp -p %s %s'%(sh, bak_path))
+        res = os.system('cp -p %s %s'%(sh, new_path))
         if res!=0:
             sys.exit(1)
         changed_list.append(sh)
