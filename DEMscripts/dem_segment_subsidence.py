02b189f43fc3a6f3482c3997383c76bf56799882
Lingcao Huang <huanglingcao@gmail.com>
2021-03-25 08:41:33 -0600

    add more parameters

--- DEMscripts/dem_segment_subsidence.py
+++ DEMscripts/dem_segment_subsidence.py
@@ -124,6 +124,8 @@ def get_dem_subscidence_polygons(in_shp, dem_diff_tif, dem_diff_thread_m=-0.5, m
 
         remain_polyons.append(poly)
 
+
+    # we should only merge polygon with similar reduction, but we already remove polygons with mean reduction > threshhold
     # merge touch polygons
     adjacent_matrix = vector_features.build_adjacent_map_of_polygons(remain_polyons)
 
@@ -166,6 +168,8 @@ def segment_subsidence_grey_image(dem_diff_grey_8bit, dem_diff, save_dir,process
     :return:
     '''
 
+    io_function.is_file_exist(dem_diff_grey_8bit)
+
     # get initial polygons
     segment_shp_path = segment_a_grey_image(dem_diff_grey_8bit,save_dir,process_num, org_raster=dem_diff)
 
@@ -186,18 +190,25 @@ def test_get_dem_subscidence_polygons():
 
 def main(options, args):
 
-    img_path = args[0]
-    io_function.is_file_exist(img_path)
+    dem_diff_path = args[0]
+    io_function.is_file_exist(dem_diff_path)
     save_dir = options.save_dir
-    # segment_subsidence_on_dem_diff(img_path,save_dir)
+    process_num = options.process_num
+    # segment_subsidence_on_dem_diff(dem_diff_path,save_dir)
 
-    segment_subsidence_grey_image()
+    dem_diff_grey_8bit = options.dem_diff_8bit
+    ele_diff_thr = options.ele_diff_thr
+    min_area = options.min_area
+    max_area = options.max_area
+
+    segment_subsidence_grey_image(dem_diff_grey_8bit, dem_diff_path, save_dir, process_num, subsidence_thr_m=ele_diff_thr,
+                                  min_area=min_area, max_area=max_area)
 
     pass
 
 if __name__ == "__main__":
 
-    usage = "usage: %prog [options] image_path "
+    usage = "usage: %prog [options] dem_diff_tif "
     parser = OptionParser(usage=usage, version="1.0 2021-2-21")
     parser.description = 'Introduction: segment subsidence based on DEM difference  '
 
@@ -205,9 +216,26 @@ if __name__ == "__main__":
                       action="store", dest="save_dir",default='./',
                       help="the folder to save results")
 
+    parser.add_option("-g", "--dem_diff_8bit",
+                      action="store", dest="dem_diff_8bit",
+                      help="the normalized elevation difference with 8bit")
+
     parser.add_option("", "--process_num",
                       action="store", dest="process_num", type=int, default=1,
-                      help="number of processes to create the mosaic")
+                      help="number of processes for image segmentation")
+
+    parser.add_option("-t", "--ele_diff_thr",
+                      action="store", dest="ele_diff_thr", type=float, default=-0.5,
+                      help="the threshold for elevation reduction ")
+
+    parser.add_option("-l", "--min_area",
+                      action="store", dest="min_area", type=float, default=40,
+                      help="the minimum area for each elevation reduction polygon")
+
+    parser.add_option("-u", "--max_area",
+                      action="store", dest="max_area", type=float, default=100000000,  # 10 km by 10 km
+                      help="the maximum area for each elevation reduction polygon")
+
 
     (options, args) = parser.parse_args()
 
