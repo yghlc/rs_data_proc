5885492cad0aee9ec75a8bbb34d0dc2d36e6f29e
Lingcao Huang <huanglingcao@gmail.com>
2021-04-26 14:46:50 -0600

    ndwi ndvi

--- landsatScripts/landsat8_ndvi_ndwi.sh
+++ landsatScripts/landsat8_ndvi_ndwi.sh
@@ -11,29 +11,23 @@
 # Exit immediately if a command exits with a non-zero status. E: error trace
 set -eE -o functrace
 
-function landsat8_ndvi () {
-    b5=$1
-    b4=$2
+function landsat8_2band_index () {
+    a=$1
+    b=$2
     out=$3
-    gdal_calc.py --calc="(A-B)/(A+B)" --outfile=${out} -A ${b5} -B ${b4} --type=Float32
+    gdal_calc.py --calc="(A-B)/(A+B)" --outfile=${out} -A ${a} -B ${b} --type=Float32
 }
 
-function landsat8_ndvi () {
-    b5=$1
-    b6=$2
-    out=$3
-    gdal_calc.py --calc="(A-B)/(A+B)" --outfile=${out} -A ${b5} -B ${b6} --type=Float32
-}
 
 dir=~/Data/Arctic/YK_delta_permafrost_depth/LC08_L1TP_078017_20130617_20170309_01_T1
 out=${dir}/LC08_L1TP_078017_20130617_20170309_ndvi.tif
-landsat8_ndvi ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B5.TIF ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B4.TIF  ${out}
+landsat8_2band_index ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B5.TIF ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B4.TIF  ${out}
 
 
 
 dir=~/Data/Arctic/YK_delta_permafrost_depth/LC08_L1TP_078017_20130617_20170309_01_T1
 out=${dir}/LC08_L1TP_078017_20130617_20170309_ndwi.tif
-landsat8_ndvi ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B5.TIF ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B6.TIF  ${out}
+landsat8_2band_index ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B3.TIF ${dir}/LC08_L1TP_078017_20130617_20170309_01_T1_B5.TIF  ${out}
 
 
 
