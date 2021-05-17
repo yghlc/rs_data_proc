9993b6c05ed5b314e97f430f249caa70083834b9
Lingcao Huang <huanglingcao@gmail.com>
2021-05-17 14:12:43 -0600

    add

---
+++ surface_water/download_water.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# introduction: downloading the extent of global surface water
+
+# run this script in Bhaltos2/lingcaoHuang/global_surface_water
+
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 17 May, 2021
+
+#https://global-surface-water.appspot.com/download
+
+# we use the tool available on Github: https://github.com/mentaljam/download_water_data 
+# to download data
+
+## show help information
+#download_water_data -h
+
+# only download the Maximum extent
+download_water_data extent
+
+
+
+
+
---
+++ surface_water/select_reprject_alaska_north.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# introduction: select a few tiles and reproject
+
+# run this script in Bhaltos2/lingcaoHuang/global_surface_water
+
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 17 May, 2021
+
+
+
+# reproject to Polar Stereographic North
+t_prj=EPSG:3413
+out_dir=extent_epsg3413
+
+# 170W_80N, 170W_70N, 160W_80N,160W_70N,150W_80N,150W_70N
+
+for tile in 170W_80N 170W_70N 160W_80N 160W_70N 150W_80N 150W_70N ;do
+  echo $tile
+  tif=$(ls extent/*${tile}*)
+  echo $tif
+
+  # re-project
+  basename=$(basename $tif)
+  output=${out_dir}/${basename}
+  echo $output
+  gdalwarp -r near -t_srs ${t_prj} -tr 30 30 -co compress=lzw $tif ${output}
+
+done
+
+
+
+
+
