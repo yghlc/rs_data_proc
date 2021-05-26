b13432f1e3938b8895c5740689446b2c54761e57
Lingcao Huang <huanglingcao@gmail.com>
2021-05-26 16:20:45 -0600

    add

---
+++ flooding_mapping/SAR_grd_files/sar_grd_prj_8bit.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## Introduction:  reprojection  and building overview images
+
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+py8=~/codes/PycharmProjects/rs_data_proc/tools/convertTo8bit.py
+
+res=10
+# US National Atlas Equal Area, do we need to reproject?
+prj=EPSG:2163
+
+dir=~/Bhaltos2/lingcaoHuang/flooding_area/Houston/Houston_pre_processed_power_transform
+input=$1
+if [ ! -z "$input" ]; then
+  dir=$input
+fi
+echo "Input dir is" $dir
+
+src_min=0.60
+src_max=0.75
+dst_min=1
+dst_max=255
+nodata=0
+
+for tif in ${dir}/*/*.tif; do
+  echo $tif
+  filename=$(basename $tif)
+  filename_noext=${filename%.*}
+
+  # reporject
+  prj_out=${filename_noext}_prj.tif
+  gdalwarp -tr ${res} ${res} -t_srs ${prj} \
+          -multi -wo NUM_THREADS=8  -r cubic $tif  ${prj_out}
+
+  # to 8bit
+  out_8bit=${filename_noext}_prj_8bit.tif
+  $py8  -s ${src_min} ${src_max} ${dst_min} ${dst_max} -n ${nodata} ${prj_out} ${out_8bit}
+
+  rm ${prj_out}
+
+done
+
+
+
+
+
+
+
