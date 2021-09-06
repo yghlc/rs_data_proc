872ed8299db84b6c9e7dc9c2ae4d9d7008a80542
Lingcao Huang <huanglingcao@gmail.com>
2021-09-06 09:42:18 -0600

    add process number

--- DEMscripts/check_valid_tif.py
+++ DEMscripts/check_valid_tif.py
@@ -27,24 +27,6 @@ from dem_common import arcticDEM_reg_tif_dir, grid_dem_diffs_dir
 import multiprocessing
 from multiprocessing import Pool
 
-# check valid tif
-def check_tif(tif_list):
-    # command_str = 'gdalinfo ' + tif_path
-    # res = os.system(command_str)
-    # print(tif_path)
-    invalid_list = []
-    for idx, tif_path in enumerate(tif_list):
-        # if idx % 50 == 0:
-        print('checking %d/%d'%(idx,len(tif_list)))
-
-        try:
-            # src = raster_io.open_raster_read(tif_path)
-            data, nodata = raster_io.read_raster_all_bands_np(tif_path)
-        except:
-            basic.outputlogMessage('invalid tif: %s'%tif_path)
-            invalid_list.append(tif_path)
-    return invalid_list
-
 def check_one_tif(idx,total,tif_path):
     try:
         print('checking %d/%d' % (idx, total))
@@ -57,23 +39,34 @@ def check_one_tif(idx,total,tif_path):
     return None
 
 
-def main():
-    process_num = multiprocessing.cpu_count()
+def main(options, args):
+
+    # process_num = multiprocessing.cpu_count()
+    process_num = options.process_num
 
     tifs = io_function.get_file_list_by_pattern(arcticDEM_reg_tif_dir, '*_dem_reg.tif')
-    save_txt_path = os.path.basename(arcticDEM_reg_tif_dir) + '_invalid_list.txt'
+    save_invalid_txt_path = os.path.basename(arcticDEM_reg_tif_dir) + '_invalid_list.txt'
+    # save_good_txt_path = os.path.basename(arcticDEM_reg_tif_dir) + '_good_list.txt'
     tif_count = len(tifs)
+
+    invalid_tif = []
 
     if process_num == 1:
         # tifs = io_function.get_file_list_by_ext('.tif',arcticDEM_reg_tif_dir, bsub_folder=False)
-        invalid_tif = check_tif(tifs)
+        for idx,tif in enumerate(tifs):
+            tif_name = os.path.basename(tif)
+            result = check_one_tif(idx,tif_count,tif)
+            if result is None:
+                pass
+            else:
+                invalid_tif.append(tif_name)
     else:
         theadPool = Pool(process_num)  # multi processes
         parameters_list = [(idx,tif_count,tif) for idx,tif in enumerate(tifs)]
         results = theadPool.starmap(check_one_tif, parameters_list)  # need python3
         invalid_tif = [ out for out in results if out is not None]
 
-    io_function.save_list_to_txt(save_txt_path, invalid_tif)
+    io_function.save_list_to_txt(save_invalid_txt_path, invalid_tif)
         
 
     # tifs = io_function.get_file_list_by_ext('.tif', grid_dem_diff_dir, bsub_folder=False)
@@ -84,5 +77,20 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    usage = "usage: %prog [options]  "
+    parser = OptionParser(usage=usage, version="1.0 2021-9-6")
+    parser.description = 'Introduction: find out invalid tifs  '
+
+    parser.add_option("-p", "--process_num",
+                      action="store", dest="process_num", type=int, default=8,
+                      help="number of processes to checking invalid tifs")
+
+    (options, args) = parser.parse_args()
+    # print(options.create_mosaic)
+
+    if len(sys.argv) < 2 or len(args) < 1:
+        parser.print_help()
+        sys.exit(2)
+
+    main(options, args)
     pass
