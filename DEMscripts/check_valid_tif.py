6524c9022a00d66ee9266cd7c0f6062010f7887e
Lingcao Huang <huanglingcao@gmail.com>
2021-09-06 09:17:42 -0600

    check invalid tifs

--- DEMscripts/check_valid_tif.py
+++ DEMscripts/check_valid_tif.py
@@ -22,14 +22,8 @@ import raster_io
 machine_name = os.uname()[1]
 
 # some folder paths
-if machine_name == 'uist':
-    arcticDEM_reg_tif_dir = '/Bhaltos2/lingcaoHuang/ArcticDEM_tmp_dir/registration_tifs'
-    grid_dem_diff_dir     = '/Bhaltos2/lingcaoHuang/ArcticDEM_tmp_dir/grid_dem_diffs'
-elif machine_name == 'ubuntu':  # tesia
-    arcticDEM_reg_tif_dir = '/home/lihu9680/Bhaltos2/lingcaoHuang/ArcticDEM_tmp_dir/registration_tifs'
-    grid_dem_diff_dir     = '/home/lihu9680/Bhaltos2/lingcaoHuang/ArcticDEM_tmp_dir/grid_dem_diffs'
-else:
-    raise ValueError('unknown machine:%s' % machine_name)
+from dem_common import arcticDEM_reg_tif_dir, grid_dem_diffs_dir
+
 
 # check valid tif
 def check_tif(tif_list):
@@ -38,11 +32,12 @@ def check_tif(tif_list):
     # print(tif_path)
     invalid_list = []
     for idx, tif_path in enumerate(tif_list):
-        if idx % 50 == 0:
-            print('checking %d/%d'%(idx,len(tif_list)))
+        # if idx % 50 == 0:
+        print('checking %d/%d'%(idx,len(tif_list)))
 
         try:
-            src = raster_io.open_raster_read(tif_path)
+            # src = raster_io.open_raster_read(tif_path)
+            data, nodata = raster_io.read_raster_all_bands_np(tif_path)
         except:
             basic.outputlogMessage(' invalid tif: %s'%tif_path)
             invalid_list.append(tif_path)
@@ -52,17 +47,19 @@ def check_tif(tif_list):
 def main():
 
     tifs = io_function.get_file_list_by_ext('.tif',arcticDEM_reg_tif_dir, bsub_folder=False)
-    invalid_tif = check_tif(tifs) 
-    for tif in invalid_tif:
-        print('removing %s'%tif)
-        io_function.delete_file_or_dir(tif)
+    invalid_tif = check_tif(tifs)
+    save_txt_path = os.path.basename(arcticDEM_reg_tif_dir) + '_invalid_list.txt'
+    io_function.save_list_to_txt(save_txt_path,invalid_tif)
+    # for tif in invalid_tif:
+    #     print('removing %s'%tif)
+        # io_function.delete_file_or_dir(tif)
         
 
-    tifs = io_function.get_file_list_by_ext('.tif', grid_dem_diff_dir, bsub_folder=False)
-    invalid_tif = check_tif(tifs)
-    for tif in invalid_tif:
-        print('removing %s'%tif)
-        io_function.delete_file_or_dir(tif)
+    # tifs = io_function.get_file_list_by_ext('.tif', grid_dem_diff_dir, bsub_folder=False)
+    # invalid_tif = check_tif(tifs)
+    # for tif in invalid_tif:
+    #     print('removing %s'%tif)
+        # io_function.delete_file_or_dir(tif)
 
 
 if __name__ == '__main__':
