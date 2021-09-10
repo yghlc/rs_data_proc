2ed51599b417aea291b3adf9a30a487c57816f15
Lingcao Huang <huanglingcao@gmail.com>
2021-09-10 16:59:09 -0600

    add

---
+++ DEMscripts/build_virtual_mosaic_for_girds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Introduction:  create a virtual mosaic for many rasters in different grids, then visualize it in QGIS.
+## in QGIS, open many rasters at once are really slow
+
+# if there are many files, it's relatively slow, but still manageable
+
+# ref: https://gis.stackexchange.com/questions/52367/handling-many-raster-files-in-qgis
+
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 10 September, 2021
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+dir=~/Data/dem_processing/dem_hillshade_newest_HWLine_grid
+ls ${dir}/*.tif > infile_list.txt
+save_vrt=~/Data/dem_processing/dem_hillshade_newest_HWLine_grid.vrt
+gdalbuildvrt -resolution average -r nearest -input_file_list infile_list.txt ${save_vrt}
+rm infile_list.txt
