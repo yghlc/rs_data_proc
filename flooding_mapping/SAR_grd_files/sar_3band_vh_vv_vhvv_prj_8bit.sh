08e6a85b3b8ef7aea993d9d394e90b57de8797fd
Lingcao Huang <huanglingcao@gmail.com>
2021-09-29 20:27:06 -0600

    projection, 8bit

---
+++ flooding_mapping/SAR_grd_files/sar_3band_vh_vv_vhvv_prj_8bit.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+## Introduction:  reprojection  and building overview images
+
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+py8=~/codes/PycharmProjects/rs_data_proc/tools/convertTo8bit.py
+
+res=10
+# US National Atlas Equal Area, do we need to reproject?
+prj=EPSG:2163
+
+dir=~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV
+input=$1
+if [ ! -z "$input" ]; then
+  dir=$input
+fi
+echo "Input dir is" $dir
+
+src_min_b1=0.70
+src_max_b1=1.0
+
+src_min_b2=0.74
+src_max_b2=1.0
+
+src_min_b3=1.0
+src_max_b3=1.0
+
+dst_min=1
+dst_max=255
+nodata=0
+
+# 98% cut histogram, min, max information
+# 20170829_RGB_composite.tif
+#0.692159, 1
+#0.740218, 1
+#1, 1
+
+# 20170829_RGB_composite_north.tif
+#0.73266, 1
+#0.788721, 1
+#1,1
+
+
+tmp=/tmp
+
+for tif in ${dir}/*/*.tif; do
+  echo $tif
+  filename=$(basename $tif)
+  filename_noext=${filename%.*}
+
+  # reporject
+  prj_out=${tmp}/${filename_noext}_prj.tif
+  gdalwarp -tr ${res} ${res} -t_srs ${prj} \
+          -multi -wo NUM_THREADS=8  -r cubic -dstnodata ${nodata} $tif  ${prj_out}
+
+  # to 8bit
+  out_8bit=${filename_noext}_prj_8bit.tif
+  $py8  -s ${src_min_b1} ${src_max_b1} ${dst_min} ${dst_max}  \
+        -s ${src_min_b2} ${src_max_b2} ${dst_min} ${dst_max} \
+        -s ${src_min_b3} ${src_max_b3} ${dst_min} ${dst_max} \
+        -n ${nodata} ${prj_out} ${out_8bit}
+
+  rm ${prj_out}
+
+done
+
+
+
