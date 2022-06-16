9e0df53a98f0fc4efb92763d6c2a3a2f905c8a8e
Lingcao Huang <huanglingcao@gmail.com>
2022-06-16 08:08:52 -0600

    output cen_points if straight line is zero

--- DEMscripts/identify_headwall_lines.py
+++ DEMscripts/identify_headwall_lines.py
@@ -121,6 +121,10 @@ def calculate_headwall_move(line_list, save_path=None, prj=None):
 
     # calculate the attributes of the line of movement
     straight_length = cen_points[0].distance(cen_points[-1])
+    if straight_length < 0.000001:
+        basic.outputlogMessage('warning, zero value of straight_length: %s'%str(straight_length))
+        basic.outputlogMessage('%s'%str(cen_points))
+        straight_length = 0.000001
     real_length = line_move.length
     sinuosity = real_length/straight_length
 
