9f46c58859726ffedbd3d9ed5ec88fe86c8717e0
yghlc <hlchuang@126.com>
2023-06-04 09:55:39 -0700

    organize and crop all results

--- snap_sar_process/test_parameters.py
+++ snap_sar_process/test_parameters.py
@@ -13,10 +13,13 @@ import os, sys
 from sklearn.model_selection import ParameterGrid
 import time
 from datetime  import datetime
+import re
 
 sys.path.insert(0, os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS'))
 import slurm_utility
 import basic_src.basic as basic
+import basic_src.io_function as io_function
+import vector_gpd
 
 machine_name = os.uname()[1]
 user_name = 'lingcao'           # default user name on computeCanada
@@ -54,6 +57,37 @@ def calculation(idx, config, sar_type, sar_list_txt, ext_shp, dem_path):
 
     return save_dir, work_dir
 
+def subset_image(input,output,bound_box_str):
+    cmd_str = 'gdalwarp -of GTiff -co tiled=yes -co bigtiff=if_safer -te %s ' % bound_box_str + input + ' ' + output
+    basic.os_system_exit_code(cmd_str)
+
+def organize_subset_coh(save_dir_list, ext_shp, out_dir=None):
+    if out_dir is None:
+        out_dir = 'all_subset'
+    if os.path.isdir(out_dir) is False:
+        io_function.mkdir(out_dir)
+
+    bounding_boxes = vector_gpd.get_vector_file_bounding_box(ext_shp)  # minx, miny, maxx, maxy
+    box_str = ' '.join([str(item) for item in bounding_boxes])
+
+    for idx, res_dir in enumerate(save_dir_list):
+        print('%d/%d: copy and subset result imagery' % (idx+1, len(save_dir_list)))
+        tif_list = io_function.get_file_list_by_pattern(res_dir, '*/*.tif')
+        para_strs = re.findall('[0-9]+', os.path.basename(res_dir))
+        para_strs = '_'.join(para_strs)
+        # subset images
+        for tif in tif_list:
+            output = os.path.join(out_dir, para_strs+'_'+os.path.basename(tif))
+            subset_image(tif, output,box_str)
+
+
+def test_organize_subset_coh():
+    w_dir = os.path.expanduser('~/Data/sar_coherence_mapping/test1/test_parameters')
+    save_dir_list = ['000_coh_results_10_03_05','001_coh_results_15_03_05']
+    ext_shp = os.path.expanduser('~/Data/Arctic/pan_Arctic/extent/SAR_coh_test_region/SAR_coh_test.shp')
+    save_dir_list = [os.path.join(w_dir,item) for item in save_dir_list]
+    print(save_dir_list)
+    organize_subset_coh(save_dir_list, ext_shp)
 
 def main():
     # get combination of parameters
@@ -82,7 +116,7 @@ def main():
         save_dir, work_dir = calculation(idx, config,sar_type, sar_list_txt, ext_shp, dem_path)
         save_dir_list.append(save_dir)
         work_dir_list.append(work_dir)
-        time.sleep(1)       # need to wait 1 second on compute canada
+        time.sleep(2)       # need to wait 2 second on compute canada
 
     while True:
         submit_job_count = slurm_utility.get_submit_job_count(user_name)
@@ -93,16 +127,9 @@ def main():
             break
 
     # organize results
-
-    # # read miou and remove some files
-    # over_miou_list = []
-    # for work_dir, para_file in zip(work_dir_list, para_file_list):
-    #     overall_miou = get_overall_miou_after_training(work_dir, para_file)
-    #     over_miou_list.append(overall_miou)
-    #     # remove_files(work_dir)
-    #
-    #     print('overall miou',os.path.basename(work_dir), overall_miou)
+    organize_subset_coh(save_dir_list, ext_shp, out_dir='all_results_crop')
 
 
 if __name__ == '__main__':
+    # test_organize_subset_coh()
     main()
