4a9f464bfbe68160eb5707fd373fe1dc2144e061
yghlc <hlchuang@126.com>
2023-06-26 16:50:33 -0700

    add

---
+++ coregistration_siftGPU/build_run_docker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+############################################################
+## build and manage docker images
+#build docker image, repository name must be lowercase
+docker build -t  siftgpu-image-match .
+
+#lists the images you have locally.
+docker images
+
+#remove local image, You can refer to an image by its ID or its name  (-f: force)
+docker rmi  -f siftgpu-image-match 
+
+
+
+############################################################
+## run and manage docker containers
+
+# create and run container, --rm: remove the container on exit
+docker run --rm -it siftgpu-image-match 
+
+# run, mount /data (-v PATH-on-host-machine:PATH-inside-container)
+# on UVic desktop
+docker run -v /data/LingcaoHuang:/data -v $HOME:/home/hlc  -it siftgpu-image-match 
+docker run --rm -v /data/LingcaoHuang:/data -v $HOME:/home/hlc  -it siftgpu-image-match 
+# on my Laptop:
+docker run --rm -v ${HOME}/Data:/data -v ${HOME}:/home/hlc  -it siftgpu-image-match 
+
+
+# tag and push to docker hub
+docker tag siftgpu-image-match siftgpu-image-match:v1
+docker push siftgpu-image-match siftgpu-image-match:v1
+
+# using GPU
+# Since Docker 19.03, you need to install nvidia-container-toolkit package and then use the --gpus all flag.
+
+### launch a new terminal to the container, e9ef58868d14 is the container by "nvidia-docker ps" or "nvidia-docker ps -a"
+# docker exec -it 0659b37a0deb bash
+
+### start the container at the background
+#4cc63f4a50d1 is got by "nvidia-docker ps -q -l"
+# docker start e9ef58868d14
+
+### attach to the container
+# docker attach e9ef58868d14
+
+
+# use CMD to set default program to run when nothing input. 
---
+++ coregistration_siftGPU/coregistration.md
@@ -0,0 +1,25 @@
+## Using SIFTGPU for Co-registration of remote sensing images
+
+I developed a program to co-registrator remote sensing images based on SIFTGPU in 2013. 
+The software environment has changed a lot. 
+Here, we built a docker container, with Ubuntu 16.04 in it, then install SIFTGPU and the program 
+I developed. 
+
+
+
+### Install SIFTGPU
+[SIFTGPU](https://github.com/pitzer/SiftGPU) is GPU implement of the [SIFT](https://link.springer.com/article/10.1023/B:VISI.0000029664.99615.94) 
+algorithm.
+We will build a docker container to install it. 
+
+There is a GitHub repo about installing SiftGPU on Ubuntu 16.04: [link](https://github.com/wangq95/SiftGPU_Linux)
+
+
+### Build and Install ImageMatchsiftGPU
+
+### Running
+
+
+
+    
+    
