59bce78a7990104dc93c1b665535c082fa882fcb
Lingcao Huang <huanglingcao@gmail.com>
2023-12-20 10:48:28 +0800

    extract dem diff polygons

--- DEMscripts/select_grids_data_overlap_vectors.py
+++ DEMscripts/select_grids_data_overlap_vectors.py
@@ -19,6 +19,8 @@ import pandas as pd
 import geopandas as gpd
 import vector_gpd
 
+from datetime import datetime
+
 arcticDEM_res_dir ="/Volumes/Seagate8T/ArcticDEM_results"
 
 def find_grids_overlap_vector_shp(grid_indexes_shp,vector_shp_list):
@@ -75,6 +77,58 @@ def find_dem_difference_raster(sel_grids_gpd, save_txt_path):
     io_function.save_list_to_txt(save_txt_path, dem_difference_file_list)
 
 
+def find_copy_dem_diff_polygons(sel_grids_gpd, save_folder):
+    # find and crop polygons of elevation reductions from tarballs
+    # to save disk space, I merged and compress polygons in each ext, then save to tarball
+
+    grid_id_list = sel_grids_gpd['cell_id'].to_list()
+    grid_polys = sel_grids_gpd['geometry'].to_list()
+    fileurl_list = sel_grids_gpd['fileurl'].to_list()
+    ext_num_list = [item.split('/')[1] for item in fileurl_list]
+
+    ele_diff_poly_dir = os.path.join(arcticDEM_res_dir,'all_grid_dem_diffs_segment_results')
+    # group grids, based on ext num
+    ext_grid_ids = {}
+    ext_grid_polys = {}
+    for grid_id, ext_num, poly in zip(grid_id_list, ext_num_list, grid_polys):
+        ext_grid_ids.setdefault(ext_num, []).append(grid_id)
+        ext_grid_polys.setdefault(ext_num, []).append(poly)
+    # print(ext_grids)
+
+    # find and crop
+    for key in ext_grid_ids.keys():
+        print('finding and cropping polygons of elevations for ext: %s '%key)
+        ext_grid_id_list = ext_grid_ids[key]
+        ext_grid_list = ext_grid_polys[key]
+
+        save_folder = '%s_grid_dem_diffs_segment_results' % key
+        if os.path.isdir(save_folder):
+            print('warning, folder for %s already exist, data may already there, skip'%key)
+            continue
+
+        # find the tarball
+        gz_files = io_function.get_file_list_by_pattern(ele_diff_poly_dir,'%s_*dem_diff_segments*.gz'%key)
+        print(gz_files)
+        if len(gz_files) != 1:
+            raise ValueError('Find %d gz files in %s, should be one '%(len(gz_files), ele_diff_poly_dir))
+        # copy and unzip the tar ball
+        gpkg_folder = io_function.unpack_tar_gz_file(gz_files[0], './')
+
+        gpkg_path = io_function.get_file_list_by_pattern(gpkg_folder,'*.gpkg')[0]
+        if os.path.isfile(gpkg_path) is False:
+            raise IOError('%s does not exist'%gpkg_path)
+
+        io_function.mkdir(save_folder)
+        # crop and save polygons in each grid
+        for g_id, g_poly in zip(ext_grid_id_list, ext_grid_list):
+            print(datetime.now(), 'crop polygons for grid: %d'%g_id)
+            save_poly_path = os.path.join(save_folder,"dem_diffs_polygons_grid%d.gpkg"%g_id)
+            vector_gpd.clip_geometries(gpkg_path,save_poly_path,g_poly,format='GPKG')
+
+        # remove the gpkg
+        io_function.delete_file_or_dir(gpkg_folder)
+
+
 def test_find_grids_overlap_vector_shp():
     data_dir = os.path.expanduser('~/Data/dem_processing/products_derived_from_ArcticDEM/Index_shp')
     grid_indexes_shp = os.path.join(data_dir,'elevation-differences_Index/elevation-differences_Index.shp')
@@ -93,6 +147,10 @@ def main(options, args):
     if len(vector_shp_list) < 1:
         raise ValueError("No input vector files")
 
+    global  arcticDEM_res_dir
+    if options.arcticDEM_res_dir is not None:
+        arcticDEM_res_dir = options.arcticDEM_res_dir
+
     #   cell_id (Integer64) = 58601
     #   tarball (String) = dem_diffs_2m_grid58601.tar.gz
     #   fileurl (String) = elevation-differences/ext11/dem_diffs_2m_grid58601.tar.gz
@@ -106,6 +164,8 @@ def main(options, args):
     save_grids_fileurl_to_txt(sel_grids_gpd,'select_grids_fileurls.txt')
     find_dem_difference_raster(sel_grids_gpd, 'select_dem_diff_raster_list.txt')
 
+    save_folder = 'ele_dem_polygons'
+    find_copy_dem_diff_polygons(sel_grids_gpd, save_folder)
 
 
 if __name__ == '__main__':
@@ -113,9 +173,13 @@ if __name__ == '__main__':
     parser = OptionParser(usage=usage, version="1.0 2023-12-18")
     parser.description = 'Introduction: select grid data (DEM diff, polygons etc) if they overlap input polygons or points '
 
-    parser.add_option("-d", "--save_dir",
+    parser.add_option("-s", "--save_dir",
                       action="store", dest="save_dir", default='select_grid_data',
                       help="the folder to save DEMs")
+
+    parser.add_option("-d", "--arcticDEM_res_dir",
+                      action="store", dest="arcticDEM_res_dir",
+                      help="the folder that contains ArcticDEM results")
 
     # test_find_grids_overlap_vector_shp()
     # sys.exit(0)
