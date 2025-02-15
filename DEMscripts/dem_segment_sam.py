9d99e9c6acffeda362fdbfbfb6aec7fa1c71ceef
Lingcao Huang <hlchuang@126.com>
2025-02-15 10:21:20 +0800

    select some regions for processing

--- DEMscripts/dem_segment_sam.py
+++ DEMscripts/dem_segment_sam.py
@@ -227,11 +227,20 @@ def main():
     org_dir = os.getcwd()
     basic.outputlogMessage(f'current directory to {org_dir}')
 
+    # select_ext_list = ['ext00','ext01','ext02','ext03','ext04','ext05','ext06','ext07','ext08','ext09']
+    select_ext_list = ['ext00','ext01','ext02','ext03','ext04']
+
     dem_diff_dir_list = io_function.get_file_list_by_pattern(ArcticDEM_results_dir,'ext??_*/*diffs*')
     for dem_diff_dir in dem_diff_dir_list:
         os.chdir(org_dir)   # change to original dir, because inside sam_segment_a_big_region, they change to other folder
         basic.outputlogMessage(f'processing {dem_diff_dir}')
         work_folder = os.path.basename(os.path.dirname(dem_diff_dir))
+        # select region
+        work_folder_begin_str = work_folder[:5]
+        if work_folder_begin_str not in select_ext_list:
+            basic.outputlogMessage(f'{work_folder} is not in the selected list, skip')
+            continue
+
         save_dir = os.path.join(ArcticDEM_results_dir, work_folder, 'grid_dem_diffs_sam_results')
         tmp_save_dir = os.path.join(tmp_dir, work_folder)
 
