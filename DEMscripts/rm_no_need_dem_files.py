9fa06e6fa439072ce20bd1974c82a99753cde45d
Lingcao Huang <yghlc@users.noreply.github.com>
2026-01-10 11:14:55 +0800

    move to backup folder

--- DEMscripts/rm_no_need_dem_files.py
+++ DEMscripts/rm_no_need_dem_files.py
@@ -14,16 +14,17 @@ deeplabforRS =  os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS')
 sys.path.insert(0, deeplabforRS)
 import basic_src.io_function as io_function
 import basic_src.basic as basic
+import basic_src.timeTools as timeTools
 
 
 from process_largeRegion_butLimited_storage import remove_no_need_dem_files
 from process_largeRegion_butLimited_storage import update_complete_grid_list
 
 from dem_common import get_grid_id_from_path
+from dem_common import arcticDEM_reg_tif_dir, grid_dem_diffs_dir
 
-def remove_on_need_stac_dem_after_DEM_diff():
+def remove_on_need_stac_dem_after_DEM_diff(bak_dir=None):
     # remove no need dem files after the DEM diff has been produced
-    from dem_common import arcticDEM_reg_tif_dir, grid_dem_diffs_dir
     dem_diff_list = io_function.get_file_list_by_pattern(grid_dem_diffs_dir,'*DEM_diff*.tif')
     grid_ids = [ get_grid_id_from_path(item) for item in dem_diff_list]
 
@@ -31,9 +32,16 @@ def remove_on_need_stac_dem_after_DEM_diff():
     for g_id in grid_ids:
         tif_list = io_function.get_file_list_by_pattern(arcticDEM_reg_tif_dir,f'*_grid{g_id}_*.tif')
         if len(tif_list) > 0:
-            basic.outputlogMessage(f'To remove {len(tif_list)} for grid: {g_id}')
-            for tif in tif_list:
-                io_function.delete_file_or_dir(tif)
+            if bak_dir is None:
+                # remove them directly
+                basic.outputlogMessage(f'To remove {len(tif_list)} tifs for grid: {g_id}')
+                for tif in tif_list:
+                    io_function.delete_file_or_dir(tif)
+            else:
+                # move to the backup directory
+                basic.outputlogMessage(f'To move {len(tif_list)} tifs for grid: {g_id} to folder: {bak_dir}')
+                for tif in tif_list:
+                    io_function.movefiletodir(tif,bak_dir,overwrite=False,b_verbose=False)
 
     basic.outputlogMessage('completed: remove_on_need_stac_dem_after_DEM_diff')
 
@@ -47,8 +55,11 @@ def main():
     # this remove tarball and tif download from PGC.
     # remove_no_need_dem_files()
 
+    backup_folder = os.path.join(arcticDEM_reg_tif_dir, f'rm_{timeTools.get_now_date_str()}')
+    io_function.mkdir(backup_folder)
+
     # remove the file download by stac
-    remove_on_need_stac_dem_after_DEM_diff()
+    remove_on_need_stac_dem_after_DEM_diff(bak_dir=backup_folder)
 
 if __name__ == '__main__':
     main()
