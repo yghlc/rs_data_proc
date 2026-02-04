cb0bce7d81ef1dfe6af918f40564d8329705475f
Lingcao Huang <yghlc@users.noreply.github.com>
2026-02-04 09:23:21 +0800

    output file count

--- DEMscripts/rename_dem_file_from_pDEMtools.py
+++ DEMscripts/rename_dem_file_from_pDEMtools.py
@@ -26,25 +26,30 @@ def rename_dem_files_in_one_grid_dir(grid_folder):
     if not os.path.isdir(grid_folder):
         basic.outputlogMessage(f'Warning, {grid_folder} is not a directory')
         return
-    basic.outputlogMessage(f'Rename files in {grid_folder}')
     tif_list = io_function.get_file_list_by_ext('.tif', grid_folder, bsub_folder=False)
     json_list = io_function.get_file_list_by_ext('.json', grid_folder, bsub_folder=False)
     if len(tif_list) != len(json_list):
         raise ValueError('The count of tif and json files is different')
 
     # rename tif files
+    tif_count = 0
     for tif in tif_list:
         if tif.endswith('_dem.tif'):
             continue
         tif_new = io_function.get_name_by_adding_tail(tif, 'dem')
         io_function.move_file_to_dst(tif, tif_new, overwrite=False, b_verbose=False)
+        tif_count += 1
 
     # rename json files
+    json_count = 0
     for j_file in json_list:
         if j_file.endswith('_dem.json'):
             continue
         json_new = io_function.get_name_by_adding_tail(j_file, 'dem')
         io_function.move_file_to_dst(j_file, json_new, overwrite=False, b_verbose=False)
+        json_count += 1
+
+    basic.outputlogMessage(f'Rename {tif_count} tif and {json_count} json files in {grid_folder}')
 
 def main(options, args):
     if len(args) > 0:
